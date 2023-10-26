0e1262b453777c146c0f789472ffa7c05955b8d0
ton-rodrigues <ewertao@icloud.com>
2023-10-26 17:08:46 -0300

    iniciando os trabalhos

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-//começando
+//primeiros testes
 
 let nome = "Tonzera"
 
 console.log("SALVE MUNDÃO")
+
+// começando o desafio
+
+let herois = [
+    ["Mônica" , 5000],
+    ["Cebolinha" , 3000],
+    ["Cascão" , 4000],
+    ["Magali" , 6000],
+    ["Chico Bento" , 3000],
+    ["Zé Esquecido" , 9999],
+    ["Do Contra" , 7600],
+    ["Franjinha" , 8000]
+]
+
+
+//console.log(
+    "O level do herói " +
+    herois[1][0] + " está no Level " +
+    herois[1][1]
